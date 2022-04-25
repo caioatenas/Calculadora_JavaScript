675cd3235ecc05ff305d8a8cf1545448ebce94a8
caioatenas <caio-san@hotmail.com>
2022-04-24 23:23:19 -0300

    24/04/2022
    refazendo o calccontroller pois possui um erro que não consegui identificar

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,36 +1,25 @@
-class CalcController {
+class CalcController{
 
     constructor(){
-        this._ displayCalcEl = document.querySelector("#display");
-        this._ dateEl = document.querySelector("#data");
-        this._ timeEl = document.querySelector("#hora");
+
+        this._displayCalcEl = document.querySelector("#display");
+        this._dataEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._displayCalcEl ="";
         this._currentDate;
-        this.initialize();
+        this.initialize().;
+
     }
 
     initialize(){
-;
-
-        this._dateEl.innerHTML = "09/12/1993";
-        this._timeEl.innerHTML = "17:30";
 
 
+        dataEl.innerHTML = "01/01/2022";
+        timeEl.innerHTML = "00:00";
     }
 
-    get displayCalc(){
-        return this._displayCalcEl.innerHTML;
+    get displayCalcEl(){
+        return this._displayCalcEl;
     }
 
-    set displayCalc(valor){
-        this._displayCalcEl.innerHTML = valor; 
-    }
-
-    get currentDate(){
-        return this._currentDate;
-
-    }
-    set currentDate(valor){
-        return this._currentDate = valor;
-    }
-    
 }
